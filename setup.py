e4b15c29a5016a574975500d281ee069d242086a
laxmisharvan <nageshgugulothu@gmail.com>
2023-07-08 10:27:52 +0530

    data ingestion and logger file is created

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_EDOT = "-e ."
+HYPEN_E_DOT = "-e ."
 def get_requirements(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as f:
         requirements =f.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
 
-        if HYPEN_EDOT in requirements:
-            requirements.remove(HYPEN_EDOT)
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
     return requirements
 
-
+6
 setup(
     name = 'mlops_project',
     version= '0.0.1',
@@ -22,3 +22,5 @@ setup(
     packages = find_packages(),
     install_requires = get_requirements("requirements.txt")
 )
+
+
---
+++ src/components/data_ingestion.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import pandas as pd
+from dataclasses import dataclass
+
+from src.exception import CustomException
+from src.logger import logging
+
