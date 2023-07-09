9901af6081449067ce92a87bc211ebf4f554ad1e
laxmisharvan <nageshgugulothu@gmail.com>
2023-07-09 19:17:35 +0530

    updated code

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,3 +6,41 @@ from dataclasses import dataclass
 from src.exception import CustomException
 from src.logger import logging
 
+from sklearn.model_selection import train_test_split
+
+@dataclass
+class DataIngestionConfig():
+    train_data_path:str = os.path.join("artifacts", "train_data.csv")
+    test_data_path:str = os.path.join("artifacts", "test_data.csv")
+    raw_data_path:str = os.path.join("artifacts", "raw_data_csv")
+
+
+class DataIngestion():
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+
+    def initiate_data_ingestion(self):
+        logging.info("Starting data ingestion")
+        try:
+            df = pd.read_csv(os.path.join("./notebook/data/gemstone.csv"))
+            logging.info("reading data is completed")
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path)
+
+            logging.info("train Test Split is intiated")
+            train_set, test_set =train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            logging.info("data ingestion is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+
+        except Exception as e:
+            raise CustomException(e,sys)
